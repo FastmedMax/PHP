664eb548f8e32647a4c8f99818b8f83567494838
Fastmed <maks.memelige@mail.ru>
2022-11-26 15:46:15 +0200

    Changed class `Student`

--- college/Education/Student.php
+++ college/Education/Student.php
@@ -4,25 +4,47 @@ namespace College\Education;
 use \DateTime;
 
 class Student extends \College\Entities\Human {
+    enum State
+    {
+        case studies;
+        case expelled;
+        case holiday;
+    }
+
     private ?string $middleName = null;
-    private bool $deduction = false;
-    private ?DateTime $date_of_deduction = null;
-    private ?string $reason_of_deduction = null;
+    private ?string $reasonOfDeduction = null;
+    private State $state = studies;
+    private bool $isHeadman = false;
+    private ?DateTime $dateOfDeduction = null;
+    private ?DateTime $dateOfUpdate = null;
+    private ?DateTime $dateOfDelete = null;
 
     public function __construct(
         private string $firstName,
         private string $lastName,
-        private DateTime $birthDate,
         private string $gender,
-        // private Group $group,
-        private DateTime $year_of_admission
+        private int $age,
+        private DateTime $birthDate,
+        private DateTime $yearOfAdmission,
+        private DateTime $assignedDate,
+        private Group $group
     ) {}
 
     public function Deduct(string $reason) : void
     {
         $this->deduction = true;
-        $this->date_of_deduction = new DateTime();
-        $this->reason_of_deduction = $reason;
+        $this->dateOfDeduction = new DateTime();
+        $this->reasonOfDeduction = $reason;
+    }
+
+    public function setGroup(InterfaceGroups $group): void
+    {
+        $this->group = $group;
+    }
+
+    public function getGroup():GroupInterface
+    {
+        return $this->group;
     }
 
 }
